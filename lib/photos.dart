f7e682a57291999716318e55907f42f7435dfb36
AggelosMitrokotsas <94068239+AggelosMitrokotsas@users.noreply.github.com>
2022-03-11 18:32:59 +0200

    Update photos.dart

--- lib/photos.dart
+++ lib/photos.dart
@@ -1,104 +1,99 @@
 import 'package:flutter/material.dart';
 import 'package:promracing/services/auth.dart';
 import 'package:promracing/theme.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'dart:math';
+import 'package:promracing/theme.dart';
+import 'package:promracing/logo.dart';
+import 'package:promracing/buttons.dart';
+import 'package:promracing/cardlist.dart';
 
+import 'package:promracing/mainpagewidget.dart';
 
 class PhotosWidget extends StatefulWidget {
-  const Photos({Key? key}) : super(key: key);
+  const PhotosWidget({Key? key}) : super(key: key);
   static const String routeName = "/Photos";
   @override
   // ignore: no_logic_in_create_state
   State<StatefulWidget> createState() => _PhotosState();
-  
-  
 }
 
 class _PhotosState extends State<PhotosWidget> {
- final AuthService _auth = AuthService();
+  final AuthService _auth = AuthService();
 
   @override
   Widget build(BuildContext context) {
-
-    return
-      Scaffold(
-                    backgroundColor: Colors.white,
-                    body: Column(
-     
-                        children: wrapper(context, images1(), _auth),
-                        )
-                  ) ;
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: Column(
+          children: wrapper(context, images1(), _auth),
+        ));
   }
 }
-images1(){
-      List<Map<dynamic, dynamic>> lists = [];
+
+images1() {
+  List<Map<dynamic, dynamic>> lists = [];
   final dbRef = FirebaseFirestore.instance.collection("photos");
 
-   return   StreamBuilder<QuerySnapshot>(
-     stream : dbRef.snapshots(),
-     builder: (context, snapshot) {
-       if(snapshot.hasData){
-         return  _buildImage(snapshot.data);
-       }
-       
-       return const LinearProgressIndicator();
-     }
-   );
+  return StreamBuilder<QuerySnapshot>(
+      stream: dbRef.snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return _buildImage(snapshot.data);
+        }
+
+        return const LinearProgressIndicator();
+      });
 }
 
-Widget _buildImage {
-    return ListView.builder(
-    
-    itemCount: snapshot?.docs.length ?? 0,
-    itemBuilder: (context, index){
-      final doc = snapshot?.docs[index];
-      return SizeBox (
-      height: 280,
-      width: 250,
-      child: Stack (
-
-                    children: <Widget>[
-  
-                        Positioned(
-                          left: 30,
-                          top: 30,
-                          child: Container(
-                            height: 200,
-                            width: 290,
-                            child: Material(
-                              type: MaterialType.transparency,
-                              child: InkWell(
-                              child: Ink.image(
-                                image: NetworkImage( doc["image"]),
-                                 padding: const EdgeInsets.all(0.0),
-                                ),
-                            
-                             onTap: () {
-
-                            },
-                            ) ,)
-                          ),
-                        ), 
-                        Positioned(
-                          top: 20,
-                          left: 20,
-                          child: Container(
-                            alignment: Alignment.center,
-                            width: 150,
-                            height: 25,
-                            decoration: const BoxDecoration(
-                              borderRadius: BorderRadius.all(const Radius.circular(20)),
-                              color: Colors.red,
-
-                             ),
-                            child:Text(doc["title"]), 
-                          ),
-                         )
-                       
-                             ),
-                      ], 
-                    ),
-                  );
-    }
+Widget _buildImage(QuerySnapshot? snapshot) {
+  return ListView.builder(
+      itemCount: snapshot?.docs.length ?? 0,
+      itemBuilder: (context, index) {
+        final doc = snapshot?.docs[index];
+        return SizedBox(
+          height: 280,
+          width: 250,
+          child: Stack(
+            children: <Widget>[
+              Positioned(
+                left: 30,
+                top: 30,
+                child: Container(
+                    height: 200,
+                    width: 290,
+                    child: Material(
+                      type: MaterialType.transparency,
+                      child: InkWell(
+                        child: Ink.image(
+                          image: NetworkImage(doc!["image"]),
+                          padding: const EdgeInsets.all(0.0),
+                        ),
+                        onTap: () {},
+                      ),
+                    )),
+              ),
+              Positioned(
+                top: 20,
+                left: 20,
+                child: Container(
+                  alignment: Alignment.center,
+                  width: 150,
+                  height: 25,
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.all(const Radius.circular(20)),
+                    color: Colors.red,
+                  ),
+                  child: Text(doc["title"]),
+                ),
+              )
+            ],
+          ),
+        );
+      });
 }
-
-
