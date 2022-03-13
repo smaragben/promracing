6e0950fd331ffd66a53dee38ad06bcd2c67df46d
AggelosMitrokotsas <el18197@mail.ntua.gr>
2022-03-13 12:57:09 +0200

    4th argument in wrapper

--- lib/mainpagewidget.dart
+++ lib/mainpagewidget.dart
@@ -3,45 +3,24 @@ import 'package:promracing/services/auth.dart';
 import 'package:promracing/theme.dart';
 import 'package:promracing/cardlist.dart';
 
-
 class MainPageWidget extends StatefulWidget {
   const MainPageWidget({Key? key}) : super(key: key);
   static const String routeName = "/MainPage";
   @override
   // ignore: no_logic_in_create_state
   State<StatefulWidget> createState() => _MainPageWidgetState();
-  
-  
 }
 
 class _MainPageWidgetState extends State<MainPageWidget> {
- final AuthService _auth = AuthService();
+  final AuthService _auth = AuthService();
 
   @override
   Widget build(BuildContext context) {
-
-    return
-      Scaffold(
-              resizeToAvoidBottomInset:false,
-
-                    backgroundColor: Colors.white,
-                    body: Column(
-     
-                        children: wrapper(context, cards1(), _auth, 1),
-                        )
-                  ) ;
+    return Scaffold(
+        resizeToAvoidBottomInset: false,
+        backgroundColor: Colors.white,
+        body: Column(
+          children: wrapper(context, cards1(), _auth, 1),
+        ));
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
--- lib/prom.dart
+++ lib/prom.dart
@@ -19,7 +19,7 @@ class _PromMembersState extends State<PromMembersWidget> {
     return Scaffold(
         backgroundColor: Colors.white,
         body: Column(
-          children: wrapper(context, members(), _auth, 0),
+          children: wrapper(context, members(), _auth, 2),
         ));
   }
 }
