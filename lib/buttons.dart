3a1f3a068e111839faa2871d71589c1bc1892d84
AggelosMitrokotsas <el18197@mail.ntua.gr>
2022-03-13 18:13:47 +0200

    sponsord.dart

--- lib/buttons.dart
+++ lib/buttons.dart
@@ -1,69 +1,68 @@
 import 'package:flutter/material.dart';
 import 'package:promracing/Quizzes.dart';
 
- 
-List<Widget> buttons(context, curindex){
+List<Widget> buttons(context, curindex) {
+  const List<Widget> _labels = [
+    Text('News', style: TextStyle(fontSize: 15)),
+    Text('Prom', style: TextStyle(fontSize: 15)),
+    Text('Quizzes', style: TextStyle(fontSize: 15)),
+    Text('Rules', style: TextStyle(fontSize: 15)),
+    Text('Photos', style: TextStyle(fontSize: 15)),
+    Text('Sponsors', style: TextStyle(fontSize: 15)),
+    Text('ContactUs', style: TextStyle(fontSize: 15))
+  ];
+  final List<Widget> _butt = [];
+  const _space = SizedBox(width: 10);
+  _butt.add(_space);
 
-  const List<Widget> _labels= [Text('News', style: TextStyle(fontSize: 15)), Text('Prom', style: TextStyle(fontSize: 15)), Text('Quizzes', style: TextStyle(fontSize: 15)), Text('Rules', style: TextStyle(fontSize: 15)), Text('Photos', style: TextStyle(fontSize: 15)), 
-  Text('Sponsors', style: TextStyle(fontSize: 15)), Text('ContactUs', style: TextStyle(fontSize: 15)) ]; 
-  final List<Widget> _butt=[];
-  const _space = SizedBox(width:10);
-      _butt.add(_space);
-
-  for (var i = 0; i<7; i++){
-    _butt.add(_button(_labels[i], i+1, context, curindex));
+  for (var i = 0; i < 7; i++) {
+    _butt.add(_button(_labels[i], i + 1, context, curindex));
     _butt.add(_space);
   }
   return _butt;
 }
 
-_button(text, index, context, curindex){
-  return  ButtonTheme(
-    minWidth: 70,
-    height: 30,
-    materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
-    child: Container(
-      margin: const EdgeInsets.all(0),
-      child: ElevatedButton.icon(
-                     style: ButtonStyle(
-                       shape:  MaterialStateProperty.all<RoundedRectangleBorder>( 
-                         RoundedRectangleBorder(
-                           borderRadius: BorderRadius.circular(50.0)
-                         )
-                       ) ,
-                       backgroundColor: MaterialStateProperty.all<Color>(Colors.red),
-                       foregroundColor: MaterialStateProperty.all<Color>( Colors.white),
-           
-                        elevation: MaterialStateProperty.resolveWith(
-                          (Set<MaterialState> states) {
-                            if(states.contains(MaterialState.pressed) || states.contains(MaterialState.hovered)|| states.contains(MaterialState.focused)|| index == curindex)
-                              return 13;
-                            else{
-                                return 0;
-                            }
-                          }
-                        ),
-                  
-               ),
-                      onPressed: (){},
-                      onLongPress:(){ 
-
-                        if(index == 3) Navigator.pushNamed(context, '/Quizzes');
-                         if(index == 1) Navigator.pushNamed(context, '/MainPage');
-                        if(index == 5) Navigator.pushNamed(context, '/Photos');},
-                        icon:  Padding(
-                          padding: const EdgeInsets.all(0),
-                          child: Image.asset(
-                            'assets/image$index.png',
-                            height: 25.0,
-                            width: 25.0,
-                          ))  ,
-                      label: Padding(padding: const EdgeInsets.all(0), child:text) ,
-                             
-                 
-  
-                    ),
-   ) );
-                      
-                
+_button(text, index, context, curindex) {
+  return ButtonTheme(
+      minWidth: 70,
+      height: 30,
+      materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+      child: Container(
+        margin: const EdgeInsets.all(0),
+        child: ElevatedButton.icon(
+          style: ButtonStyle(
+            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50.0))),
+            backgroundColor: MaterialStateProperty.all<Color>(
+                const Color.fromARGB(255, 255, 158, 190)),
+            foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+            elevation:
+                MaterialStateProperty.resolveWith((Set<MaterialState> states) {
+              if (states.contains(MaterialState.pressed) ||
+                  states.contains(MaterialState.hovered) ||
+                  states.contains(MaterialState.focused) ||
+                  index == curindex)
+                return 13;
+              else {
+                return 0;
+              }
+            }),
+          ),
+          onPressed: () {},
+          onLongPress: () {
+            if (index == 3) Navigator.pushNamed(context, '/Quizzes');
+            if (index == 1) Navigator.pushNamed(context, '/MainPage');
+            if (index == 5) Navigator.pushNamed(context, '/Photos');
+          },
+          icon: Padding(
+              padding: const EdgeInsets.all(0),
+              child: Image.asset(
+                'assets/image$index.png',
+                height: 25.0,
+                width: 25.0,
+              )),
+          label: Padding(padding: const EdgeInsets.all(0), child: text),
+        ),
+      ));
 }
---
+++ lib/sponsors.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'package:promracing/services/auth.dart';
+import 'package:promracing/theme.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class Sponsors extends StatefulWidget {
+  const Sponsors({Key? key}) : super(key: key);
+  static const String routeName = "/Sponsors";
+  @override
+  // ignore: no_logic_in_create_state
+  State<StatefulWidget> createState() => SponsorsState();
+}
+
+class SponsorsState extends State<Sponsors> {
+  final AuthService _auth = AuthService();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: Column(
+          children: wrapper(context, sponsors(), _auth, 2),
+        ));
+  }
+}
+
+sponsors() {
+  List<CollectionReference> list = [];
+  var dbRef = FirebaseFirestore.instance.collection("bronzeSponsors");
+  list.add(dbRef);
+  dbRef = FirebaseFirestore.instance.collection("silverSponsors");
+  list.add(dbRef);
+  dbRef = FirebaseFirestore.instance.collection("goldenSponsors");
+  list.add(dbRef);
+  dbRef = FirebaseFirestore.instance.collection("platinumSponsors");
+  list.add(dbRef);
+  dbRef = FirebaseFirestore.instance.collection("supporters");
+  list.add(dbRef);
+  dbRef = FirebaseFirestore.instance.collection("mediaSponsorship");
+  list.add(dbRef);
+
+  for (var i = 0; i < list.length; i++) {
+    return StreamBuilder<QuerySnapshot>(
+        stream: dbRef.snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return _buildImage(snapshot.data);
+          }
+
+          return const LinearProgressIndicator();
+        });
+  }
+}
+
+Widget _buildImage(QuerySnapshot? snapshot) {
+  return ListView.builder(
+      itemCount: snapshot?.docs.length ?? 0,
+      itemBuilder: (context, index) {
+        final doc = snapshot?.docs[index];
+        return SizedBox(
+          height: 280,
+          width: 250,
+          child: Stack(
+            children: <Widget>[
+              Positioned(
+                left: 30,
+                top: 30,
+                child: Container(
+                    height: 200,
+                    width: 290,
+                    child: Material(
+                      type: MaterialType.transparency,
+                      child: InkWell(
+                        child: Ink.image(
+                          image: NetworkImage(doc!["logo"]),
+                          padding: const EdgeInsets.all(0.0),
+                        ),
+                        onTap: () {},
+                      ),
+                    )),
+              ),
+              Positioned(
+                top: 20,
+                left: 20,
+                child: Container(
+                  alignment: Alignment.center,
+                  width: 150,
+                  height: 25,
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                    color: Colors.blue,
+                  ),
+                  child: Text(doc["name"]),
+                ),
+              ),
+              Positioned(
+                top: 10,
+                left: 10,
+                child: Container(
+                  alignment: Alignment.center,
+                  width: 150,
+                  height: 25,
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                    color: Colors.purple,
+                  ),
+                  child: Text(doc["type"]),
+                ),
+              )
+            ],
+          ),
+        );
+      });
+}
